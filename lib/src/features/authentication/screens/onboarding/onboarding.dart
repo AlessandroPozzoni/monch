8a6295d51432754ebdb3605a4e2afe48f7f832f9
Alessandro <alessandro.pozzoni99.ap@gmail.com>
2024-05-26 00:31:33 +0200

    Added onboarding screens and widgets.

--- lib/src/features/authentication/screens/onboarding/onboarding.dart
+++ lib/src/features/authentication/screens/onboarding/onboarding.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:monch/src/features/authentication/controllers/onboarding/onboarding.controller.dart';
 import 'package:monch/src/features/authentication/screens/onboarding/onboarding_next_button.dart';
 import 'package:monch/src/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart';
 import 'package:monch/src/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
 import 'package:monch/src/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
-import 'package:monch/src/utils/constants/colors.dart';
 import 'package:monch/src/utils/constants/image_strings.dart';
-import 'package:monch/src/utils/constants/sizes.dart';
 import 'package:monch/src/utils/constants/texts_strings.dart';
-import 'package:monch/src/utils/device/device_utility.dart';
-import 'package:monch/src/utils/helpers/helper_functions.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
