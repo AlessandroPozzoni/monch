4de48a8131f3f9100218ccd412ea8ae94f6354e2
Alessandro <alessandro.pozzoni99.ap@gmail.com>
2024-07-31 15:30:20 +0200

    Refactored onboarding controller.

--- lib/src/features/authentication/screens/onboarding/onboarding_next_button.dart
+++ lib/src/features/authentication/screens/onboarding/onboarding_next_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:iconsax/iconsax.dart';
-import 'package:monch/src/features/authentication/controllers/onboarding/onboarding.controller.dart';
+import 'package:monch/src/features/authentication/controllers/onboarding/onboarding_controller.dart';
 import 'package:monch/src/utils/constants/colors.dart';import 'package:monch/src/utils/constants/sizes.dart';
 import 'package:monch/src/utils/device/device_utility.dart';class OnBoardingNextButton extends StatelessWidget {
   const OnBoardingNextButton({
