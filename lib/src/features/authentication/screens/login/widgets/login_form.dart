b0a2678891586229a5041fad00228c28877fcf13
Alessandro <alessandro.pozzoni99.ap@gmail.com>
2024-07-31 16:13:44 +0200

    Fixed issues with controller and field placeholders.

--- lib/src/features/authentication/screens/login/widgets/login_form.dart
+++ lib/src/features/authentication/screens/login/widgets/login_form.dart
@@ -19,83 +19,86 @@ class MonchLoginForm extends StatelessWidget {
     final controller = Get.put(LoginController());
 
     return Form(
+        key: controller.loginFormKey,
         child: Padding(
-      padding: const EdgeInsets.symmetric(vertical: MonchSizes.spaceBwSections),
-      child: Column(
-        children: [
-          // Email
-          TextFormField(
-            controller: controller.email,
-            validator: (value) => MonchValidator.validateEmail(value),
-            decoration: const InputDecoration(
-                prefixIcon: Icon(Iconsax.direct_right),
-                labelText: MonchTexts.email),
-          ),
-          const SizedBox(height: MonchSizes.spaceBwInputFields),
-
-          // Password
-          Obx(
-            // Observer of the value of the hidePassword variable
-            () => TextFormField(
-              controller: controller.password,
-              validator: (value) => MonchValidator.validatePassword(value),
-              obscureText: controller.hidePassword.value,
-              decoration: InputDecoration(
-                labelText: MonchTexts.password,
-                prefixIcon: const Icon(Iconsax.password_check),
-                suffixIcon: IconButton(
-                    onPressed: () => controller.hidePassword.value =
-                        !controller.hidePassword.value,
-                    icon: Icon(controller.hidePassword.value
-                        ? Iconsax.eye_slash
-                        : Iconsax.eye)),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: MonchSizes.spaceBwInputFields / 2,
-          ),
-
-          // Remember me & Forgot password
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          padding:
+              const EdgeInsets.symmetric(vertical: MonchSizes.spaceBwSections),
+          child: Column(
             children: [
-              // Remember me
+              // Email
+              TextFormField(
+                controller: controller.email,
+                validator: (value) => MonchValidator.validateEmail(value),
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Iconsax.direct_right),
+                    labelText: MonchTexts.email),
+              ),
+              const SizedBox(height: MonchSizes.spaceBwInputFields),
+
+              // Password
+              Obx(
+                // Observer of the value of the hidePassword variable
+                () => TextFormField(
+                  controller: controller.password,
+                  validator: (value) => MonchValidator.validatePassword(value),
+                  obscureText: controller.hidePassword.value,
+                  decoration: InputDecoration(
+                    labelText: MonchTexts.password,
+                    prefixIcon: const Icon(Iconsax.password_check),
+                    suffixIcon: IconButton(
+                        onPressed: () => controller.hidePassword.value =
+                            !controller.hidePassword.value,
+                        icon: Icon(controller.hidePassword.value
+                            ? Iconsax.eye_slash
+                            : Iconsax.eye)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: MonchSizes.spaceBwInputFields / 2,
+              ),
+
+              // Remember me & Forgot password
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Obx(() => Checkbox(
-                      // Observer of the value of the hidePassword variable
-                      value: controller.rememberMe.value,
-                      onChanged: (value) => controller.rememberMe.value =
-                          !controller.rememberMe.value)),
-                  const Text(MonchTexts.rememberMe),
+                  // Remember me
+                  Row(
+                    children: [
+                      Obx(() => Checkbox(
+                          // Observer of the value of the hidePassword variable
+                          value: controller.rememberMe.value,
+                          onChanged: (value) => controller.rememberMe.value =
+                              !controller.rememberMe.value)),
+                      const Text(MonchTexts.rememberMe),
+                    ],
+                  ),
+
+                  // Forgot password
+                  TextButton(
+                      onPressed: () => Get.to(() => const ForgotPassword()),
+                      child: const Text(MonchTexts.forgotPassword)),
                 ],
               ),
+              const SizedBox(height: MonchSizes.spaceBwSections),
 
-              // Forgot password
-              TextButton(
-                  onPressed: () => Get.to(() => const ForgotPassword()),
-                  child: const Text(MonchTexts.forgotPassword)),
+              // Sign in button
+              SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () => controller.emailAndPasswordSignIn(),
+                      child: const Text(MonchTexts.signIn))),
+              const SizedBox(height: MonchSizes.spaceBwItems),
+
+              // Create account button
+              SizedBox(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                      onPressed: () => Get.to(() => const SignupScreen()),
+                      child: const Text(MonchTexts.createAccount))),
+              // const SizedBox(height: MonchSizes.spaceBwSections),
             ],
           ),
-          const SizedBox(height: MonchSizes.spaceBwSections),
-
-          // Sign in button
-          SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                  onPressed: () => controller.emailAndPasswordSignIn(), child: const Text(MonchTexts.signIn))),
-          const SizedBox(height: MonchSizes.spaceBwItems),
-
-          // Create account button
-          SizedBox(
-              width: double.infinity,
-              child: OutlinedButton(
-                  onPressed: () => Get.to(() => const SignupScreen()),
-                  child: const Text(MonchTexts.createAccount))),
-          // const SizedBox(height: MonchSizes.spaceBwSections),
-        ],
-      ),
-    ));
+        ));
   }
 }
